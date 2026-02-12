e6d1104f143397ea759aee55a3b9efffec4ad9d9
localhostinglabs <akangfc0@gmail.com>
2026-02-12 20:32:39 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import './polyfills';  // MUST be first line
+
+import { AppRegistry } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+import App from './App';
+import { name as appName } from './app.json';
+
+enableScreens();
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ polyfills.js
@@ -0,0 +1,5 @@
+// Required for TextEncoder/TextDecoder
+import 'fast-text-encoding';
+
+// Required for crypto.getRandomValues
+import 'react-native-get-random-values';
